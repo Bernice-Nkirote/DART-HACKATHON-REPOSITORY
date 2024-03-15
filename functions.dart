bb266beca9dadf07ad1326e021ef15fa99005e50
Bernice-Nkirote <bernicemwirigi@gmail.com>
2024-03-16 02:54:56 +0300

    made changes to controlflow,functions and program file

--- functions.dart
+++ functions.dart
@@ -1,2 +1,14 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+// ## Program 2: Perform Mathematical Operations with Functions
+// Write a Dart program that performs two mathematical operations using functions.
+void main() {
+  print(multiply(10.5, 2.3));
+  print(subtract(150, 80));
+}
+
+multiply(double a, double b) {
+  return a * b;
+}
+
+subtract(int c, int d) {
+  return c - d;
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,13 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//  Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main() {
+  String firstName = "Bernice";
+  String lasttName = "Mwirigi";
+  int age = 29;
+  String school = "Power Learn Project";
+  String hobby = "Painting";
+
+  print(
+      "Hi my name is $firstName $lasttName and i am $age years old and i love $hobby during my leisurely hours. I started this coding journey in 2021 and i joined $school in the year 2024. So far $school has been challenging and educative. I love it here!!");
+}
